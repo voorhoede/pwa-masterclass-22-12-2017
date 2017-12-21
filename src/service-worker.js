173daeb4a5cfaae526b449611fc012fca3355fbb
Anne Fortuin <phortuin@gmail.com>
2017-12-21 22:01:38 +0100

    Add SW basics for exercise 1

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,12 @@
-//
-// Assignment 1
-// Add event listeners
-//
+self.addEventListener('install', event => {
+    self.waitUntil(self.skipWaiting());
+    console.log('Service worker installed!');
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker ready to go!');
+});
+
+self.addEventListener('fetch', event => {
+    console.log(`🚂 ${event.request.method} request to ${event.request.url}`);
+});
